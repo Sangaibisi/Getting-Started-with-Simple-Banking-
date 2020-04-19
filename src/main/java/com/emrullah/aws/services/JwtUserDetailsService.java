b21d7a611abf8c4e4e5ef4de6cd6747a2e5bcfd5
Emrullah YILDIRIM <emrullahyildirim@windowslive.com>
2020-04-19 16:49:19 +0300

    JWT classes done.

---
+++ src/main/java/com/emrullah/aws/services/JwtUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.emrullah.aws.services;
+
+import com.emrullah.aws.dao.IUserDAO;
+import com.emrullah.aws.model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserDAO userDao;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public Users save(Users user) {
+        Users newUser = new Users();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userDao.save(newUser);
+    }
+}
